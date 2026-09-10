5461b9d0008210316e21aea998b4e933199240fb
Onur Kocabiyik <onurkcbyk@hotmail.com>
2026-09-10 22:22:07 +0300

    chore: harden database initialization

--- src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
+++ src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TravelWise.Domain.Constants;
-using TravelWise.Infrastructure.Identity;
 
 namespace TravelWise.Infrastructure.Data;
 
@@ -25,14 +24,14 @@ public class ApplicationDbContextInitialiser
 {
     private readonly ILogger<ApplicationDbContextInitialiser> _logger;
     private readonly ApplicationDbContext _context;
-    private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
 
-    public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    public ApplicationDbContextInitialiser(ILogger<ApplicationDbContextInitialiser> logger, 
+        ApplicationDbContext context, 
+        RoleManager<IdentityRole> roleManager)
     {
         _logger = logger;
         _context = context;
-        _userManager = userManager;
         _roleManager = roleManager;
     }
 
@@ -71,21 +70,5 @@ public class ApplicationDbContextInitialiser
         {
             await _roleManager.CreateAsync(administratorRole);
         }
-
-        // Default users
-        var administrator = new ApplicationUser { UserName = "administrator@localhost", Email = "administrator@localhost" };
-
-        if (_userManager.Users.All(u => u.UserName != administrator.UserName))
-        {
-            await _userManager.CreateAsync(administrator, "Administrator1!");
-            if (!string.IsNullOrWhiteSpace(administratorRole.Name))
-            {
-                await _userManager.AddToRolesAsync(administrator, new [] { administratorRole.Name });
-            }
-        }
-
-        // Default data
-        // Seed, if necessary
-
     }
 }
