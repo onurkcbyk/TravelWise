67c396b8d5d36e7c66bb27a29ff090c4a7681a94
Onur Kocabiyik <onurkcbyk@hotmail.com>
2026-09-10 21:18:13 +0300

    chore: remove template sample features

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,12 +1,6 @@
-﻿using TravelWise.Domain.Entities;
-
-namespace TravelWise.Application.Common.Interfaces;
+﻿namespace TravelWise.Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
-    DbSet<TodoList> TodoLists { get; }
-
-    DbSet<TodoItem> TodoItems { get; }
-
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
--- src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
+++ src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
@@ -1,6 +1,4 @@
 ﻿using TravelWise.Domain.Constants;
-using TravelWise.Domain.Entities;
-using TravelWise.Domain.ValueObjects;
 using TravelWise.Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -89,22 +87,6 @@ public class ApplicationDbContextInitialiser
 
         // Default data
         // Seed, if necessary
-        if (!_context.TodoLists.Any())
-        {
-            _context.TodoLists.Add(new TodoList
-            {
-                Title = "Tasks",
-                Colour = Colour.Green,
-                Items =
-                {
-                    new TodoItem { Title = "Make a todo list 📃" },
-                    new TodoItem { Title = "Check off the first item ✅" },
-                    new TodoItem { Title = "Realise you've already done two things on the list! 🤯"},
-                    new TodoItem { Title = "Reward yourself with a nice, long nap 🏆" },
-                }
-            });
 
-            await _context.SaveChangesAsync();
-        }
     }
 }
